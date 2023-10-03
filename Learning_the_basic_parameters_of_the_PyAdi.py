21a2669d1451d38bb16292441a691bac8ff98524
Marina1825 <buyan0va.m@yandex.ru>
2023-10-03 21:28:37 +0700

    add a second code for the task practice

---
+++ Learning_the_basic_parameters_of_the_PyAdi.py
@@ -0,0 +1,34 @@
+import time
+import adi
+import matplotlib.pyplot as plt
+import numpy as np
+
+sdr = adi.Pluto("ip: ****")#узнать
+sdr.rx_lo = #узнать
+
+num_iterations = 30
+
+rx_real_data = []
+rx_imag_data = []
+wait_values = []
+
+for r in range(num_iterations):# Сбор данных
+    rx = sdr.rx()
+    plt.clf()
+
+    wait = np.mean(rx.imag)
+    wait_values.append(wait)
+
+    plt.xlabel("Время")
+    plt.ylabel("Амплитуда " + str(wait))
+    plt.plot(rx.real)
+    plt.plot(rx.imag)
+    
+    plt.draw()
+    plt.pause(0.05)
+    if wait > 200:
+        time.sleep(5)
+
+    time.sleep(0.5)# Пауза
+
+plt.show()
