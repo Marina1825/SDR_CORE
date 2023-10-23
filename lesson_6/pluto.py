25efe718eba3377e288cee759c3e9b7063241dcd
Marina1825 <buyan0va.m@yandex.ru>
2023-10-23 14:10:17 +0700

    add signal construction

--- lesson_6/pluto.py
+++ lesson_6/pluto.py
@@ -7,15 +7,32 @@ import numpy as np
 sdr = adi.Pluto("ip:192.168.3.1")
 
 
-sdr.rx_lo = int(2400e6)
-sdr.tx_lo = int(2400e6)
+sdr.rx_lo = int(2000e6 + 2e6 * 3)
+sdr.tx_lo = int(2000e6 + 2e6 * 3)
 sdr.sample_rate = 1e6
-sdr.rx_buffer_size = 1000
+sdr.rx_buffer_size = 1024
 
+t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
 
-print(type(d[1]))
+signal = np.zeros(len(t))
+non_zero_samples = 200
+start_sample = 500
+signal[start_sample:start_sample+non_zero_samples] = amplitude
+
+sdr.tx_cyclic_buffer = True
+sdr.tx_enabled = True
+sdr.tx(sample)
+
+plt.plot(t, signal)
+plt.xlabel('Время (секунды)')
+plt.ylabel('Амплитуда')
+plt.title('Прямоугольный сигнал')
+plt.grid(True)
+plt.show()
+sdr.close()
+'''print(type(d[1]))
 print(str_after)
 plt.xlabel('sample')
 plt.ylabel('ampl')
 plt.plot(d)
-plt.show()    
+plt.show()    '''
